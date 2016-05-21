875bb7d9904ce576a996c1c5b3cf428bec78eec6
Hamed Ramezanian <hamed.r.nik@gmail.com>
2016-05-21 22:41:29 +0100

    Add Gemfile.lock into git ignore file

--- lib/aylien_text_api/version.rb
+++ lib/aylien_text_api/version.rb
@@ -13,5 +13,5 @@
 # limitations under the License.
 
 module AylienTextApi
-  VERSION = "0.6.0" unless defined? AylienTextApi::VERSION
+  VERSION = "0.6.1" unless defined? AylienTextApi::VERSION
 end
