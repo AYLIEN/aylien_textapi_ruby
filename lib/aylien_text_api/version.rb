39ad35f0dc81b0f14d46562163f4988e6390fa5d
snebel29 <nebel.sven@gmail.com>
2018-05-31 12:08:25 +0100

    Bump version to 0.8.0

--- lib/aylien_text_api/version.rb
+++ lib/aylien_text_api/version.rb
@@ -13,5 +13,5 @@
 # limitations under the License.
 
 module AylienTextApi
-  VERSION = "0.7.0" unless defined? AylienTextApi::VERSION
+  VERSION = "0.8.0" unless defined? AylienTextApi::VERSION
 end
